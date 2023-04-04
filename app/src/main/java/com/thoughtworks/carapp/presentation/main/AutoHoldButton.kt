bbf4f73749b9328d85cf164efc2bb82800cd5a63
XinChen <xin.chen@thoughtworks.com>
2023-04-04 11:06:28 +0800

    feat: show auto hold state in main screen

--- app/src/main/java/com/thoughtworks/carapp/presentation/main/AutoHoldButton.kt
+++ app/src/main/java/com/thoughtworks/carapp/presentation/main/AutoHoldButton.kt
@@ -1,40 +1,47 @@
 package com.thoughtworks.carapp.presentation.main
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.thoughtworks.carapp.R
 
 @Composable
-fun AutoHoldButton() {
-    var isSelected by remember { mutableStateOf(true) }
+fun AutoHoldButton(
+    modifier: Modifier = Modifier,
+    isOpened: Boolean,
+    onSwitch: () -> Unit = {}
+) {
     Box(
-        modifier = Modifier.selectable(
-            selected = isSelected,
-            onClick = {
-                isSelected = !isSelected
-            },
-            enabled = true,
-            role = Role.Image
-        ).wrapContentSize(),
+        modifier = modifier
+            .clip(RoundedCornerShape(23.dp))
+            .background(color = Color.Unspecified, shape = RoundedCornerShape(23.dp))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = LocalIndication.current,
+                onClick = onSwitch
+            )
+            .wrapContentSize(),
         contentAlignment = Alignment.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_auto_hold_bg),
+        Icon(
+            painter = painterResource(id = R.drawable.ic_auto_hold_background),
             contentDescription = ""
         )
 
@@ -47,7 +54,7 @@ fun AutoHoldButton() {
             Spacer(modifier = Modifier.height(15.dp))
             Image(
                 painter = painterResource(
-                    id = if (isSelected) {
+                    id = if (isOpened) {
                         R.drawable.ic_auto_hold_indicator_opened
                     } else {
                         R.drawable.ic_auto_hold_indicator_closed
--- app/src/main/java/com/thoughtworks/carapp/presentation/main/MainScreen.kt
+++ app/src/main/java/com/thoughtworks/carapp/presentation/main/MainScreen.kt
@@ -2,13 +2,20 @@ package com.thoughtworks.carapp.presentation.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun MainScreen() {
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        AutoHoldButton()
+fun MainScreen(viewModel: MainViewModel = viewModel()) {
+    val autoHoldState by viewModel.autoHoldUiState.collectAsState()
+
+    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
+        AutoHoldButton(modifier = Modifier.padding(bottom = 55.dp), autoHoldState)
     }
 }
