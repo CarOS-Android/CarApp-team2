78918c7f57af6e02d79e36f94e9cf512dbc9d07d
Ziyu Guo <ziyu.guo@thoughtworks.com>
2023-04-10 16:12:26 +0800

    fix: turn head or high bean light off when another light is on

--- app/src/main/java/com/thoughtworks/carapp/presentation/main/carlights/CarLightViewModel.kt
+++ app/src/main/java/com/thoughtworks/carapp/presentation/main/carlights/CarLightViewModel.kt
@@ -4,6 +4,7 @@ import com.thoughtworks.carapp.domain.CarLightUseCase
 import com.thoughtworks.carapp.presentation.base.BaseViewModel
 import com.thoughtworks.carapp.presentation.base.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 sealed interface CarLightEvent : Event {
@@ -17,8 +18,12 @@ class CarLightViewModel @Inject constructor(
     private val carLightUseCase: CarLightUseCase,
 ) : BaseViewModel<CarLightEvent>() {
     val isHazardLightOn = carLightUseCase.hazardLightFlow().stateWith(false)
-    val isHeadLightOn = carLightUseCase.headLightFlow().stateWith(false)
-    val isHighBeamLightOn = carLightUseCase.highBeamLightFlow().stateWith(false)
+    val isHeadLightOn = carLightUseCase.headLightFlow()
+        .onEach { if (it) carLightUseCase.turnOffHighBeamLight() }
+        .stateWith(false)
+    val isHighBeamLightOn = carLightUseCase.highBeamLightFlow()
+        .onEach { if (it) carLightUseCase.turnOffHeadLight() }
+        .stateWith(false)
 
     override fun handleEvents(event: CarLightEvent) {
         when (event) {
