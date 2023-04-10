a0dd31b75fb646e3323cda2f8b183e874e955daf
Zhang Guo <guo.zhang@thoughtworks.com>
2023-04-10 16:34:54 +0800

    feat: update hmi library version to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ androidApplication {
 
 dependencies {
     implementation(files("${rootProject.projectDir}/libs/car-lib-debug.aar"))
-    implementation("com.thoughtworks.blindhmi:ui:1.0.3")
+    implementation("com.thoughtworks.blindhmi:ui:1.0.5")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     implementation(libs.bundles.kotlin)
